5f0e99433f8cc566ebada3f4d6420e5d2496614c
hyewon-han <hw1635@hanmail.net>
2023-12-15 20:54:43 +0900

    setup

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,13 +3,40 @@ import styled from "styled-components";
 import theme from "../styles/Theme";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/config/configStore";
+import { useQuery } from "@tanstack/react-query";
+import { getTodos } from "../api/todos";
+
+type TodoType = {
+  id: string;
+  title: string;
+  content: string;
+  isDone: boolean;
+};
 
 type Props = {
   isDone: boolean;
 };
 
 const TodoList = ({ isDone }: Props) => {
-  const todos = useSelector((state: RootState) => state.todosSlice);
+  //   const todos = useSelector((state: RootState) => state.todosSlice);
+
+  const {
+    data: todos = [],
+    isLoading,
+    error,
+  } = useQuery<TodoType[]>({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+  console.log(todos);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/config/configStore";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </Provider>
+  </QueryClientProvider>
 );
